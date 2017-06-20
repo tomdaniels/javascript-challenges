7656dbfe04b16ae55e016c9a0e2e9e94c031b8f3
tomdaniels <thomsondaniels@hotmail.com>
2017-06-20 16:23:18 +1000

    html set up, link to script

---
+++ challenges/array-challenge/script.js
@@ -0,0 +1,8 @@
+function mergeAndSortArrays(array1, array2) {
+    // Tom's code goes here
+}
+
+var array1 = [1, 10, 100];
+var array2 = [2, 20, 200];
+var resultArray = mergeAndSortArrays(array1, array2);
+console.log(resultArray);
