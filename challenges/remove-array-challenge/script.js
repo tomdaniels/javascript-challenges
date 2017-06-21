8a0e8fe9b9c223f16cae295c4b105bc4aa9c09a5
tomdaniels <thomsondaniels@hotmail.com>
2017-06-21 17:31:54 +1000

    Remove Array -  Challenge 2

--- challenges/remove-array-challenge/script.js
+++ challenges/remove-array-challenge/script.js
@@ -1,3 +1,17 @@
+function removeNumberFromArray(myArray, number) {
+  var i = 0;
+  var newArray = [];
+  for (i = 0; i < myArray.length; i++){
+    var numberToCheck = myArray[i];
+    if (numberToCheck == number){
+      console.log("found ya matee");
+    } else {
+      newArray.push(numberToCheck);
+    }
+  }
+
+  return newArray;
+}
 var myArray = [1, 2, 3, 4, 5];
 var number = 4;
 var resultArray = removeNumberFromArray(myArray, number);
