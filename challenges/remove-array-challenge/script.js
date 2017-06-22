2b067320189e64d6e41173b074895d0c11e69dc2
tomdaniels <thomsondaniels@hotmail.com>
2017-06-22 11:18:59 +1000

    uses filter function rather than manual loop

--- challenges/remove-array-challenge/script.js
+++ challenges/remove-array-challenge/script.js
@@ -1,15 +1,25 @@
+/* Demonstrates a manual way to filter through an Array.
+*  function removeNumberFromArray(myArray, number){
+*    var i = 0;
+*    var newArray = [];
+*    for (i = 0; i < myArray.length; i++) {
+*      var numberToCheck = myArray[i];
+*      if (numberToCheck == number) {
+*        console.log('gotchya');
+*      } else {
+*        newArray.push(numberToCheck);
+*      }
+*    }
+*    return newArray;
+*   }
+*  var myArray = [1, 2, 3, 4, 5];
+*  var number = 4;
+*  var resultArray = removeNumberFromArray(myArray, number);
+*  console.log(resultArray); */
+
+// Uses filter function instead of manual loop.
 function removeNumberFromArray(myArray, number){
-  var i = 0;
-  var newArray = [];
-  for (i = 0; i < myArray.length; i++) {
-    var numberToCheck = myArray[i];
-    if (numberToCheck == number) {
-      console.log('gotchya');
-    } else {
-      newArray.push(numberToCheck);
-    }
-  }
-  return newArray;
+  return myArray.filter(function(n) {return n != number});
 }
 
 var myArray = [1, 2, 3, 4, 5];
