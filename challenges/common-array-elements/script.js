c17a05a0a7858e1130d9608283a38b27ebe7cd83
tomdaniels <thomsondaniels@hotmail.com>
2017-06-22 18:08:21 +1000

    in progress

---
+++ challenges/common-array-elements/script.js
@@ -0,0 +1,15 @@
+function commonArrayElements(array1, array2){
+  var newArray = [];
+  for (var i = 0; i < array1.length; ++i){
+      if (array1[i] === array2){
+      }
+    }
+  }
+
+
+var array1 = [1, 20, 10, 100, 2];
+var array2 = [500, 2, 20, 200];
+var resultArray = commonArrayElements(array1, array2);
+console.log(resultArray);
+
+//for (var j = 0; j < array2.length; ++i){
