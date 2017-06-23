d08d2195a1ed07ab5fb829c62b1949dd1414bab7
tomdaniels <thomsondaniels@hotmail.com>
2017-06-23 12:04:06 +1000

    Fixes function to return array instead of console.log

--- challenges/reverse-array/script.js
+++ challenges/reverse-array/script.js
@@ -1,9 +1,11 @@
-function commonArrayElements(myArray){
-  var i = 0;
-  for (i = myArray.length - 1; i >= 0; i--)
-    console.log(myArray[i]);
+function reverseArray(myArray){
+  var newArray = [];
+  for (var i = myArray.length -1; i >= 0; i--) {
+    newArray.push(myArray[i]);
+  }
+  return newArray;
 }
 
 var myArray = [1, 20, 10, 100, 2];
-var resultArray = commonArrayElements(myArray);
+var resultArray = reverseArray(myArray);
 console.log(resultArray);
