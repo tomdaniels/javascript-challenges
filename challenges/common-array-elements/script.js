cc91a30e7157b147ea753776e3e2ac881a1f58f5
tomdaniels <thomsondaniels@hotmail.com>
2017-06-23 17:20:37 +1000

    still in progress

--- challenges/common-array-elements/script.js
+++ challenges/common-array-elements/script.js
@@ -1,11 +1,25 @@
 function commonArrayElements(array1, array2){
   var results = [];
   for (var i = 0; i < array1.length; ++i){
-      if (array1[i] === array2){
-      } results.push(resultArray);
-    }
+    array2.filter(function(array1){
+      if (array1[i] === array2[i]){
+        results.push(array1, array2);
+      };
+    })
   }
+  array2.concat(array1);
+  return results;
+}
 
+
+
+/*
+function commonArrayElements(array1, array2){
+  for (var i = 0; i < array1.length; i++){
+    array2.filter(function(value) {return value != array1[i]});
+  }
+}
+*/
 
 var array1 = [1, 20, 10, 100, 2];
 var array2 = [500, 2, 20, 200];
