91b52344b979191f0f75504701cc51180ddba883
tomdaniels <thomsondaniels@hotmail.com>
2017-06-26 15:18:44 +1000

    Uses map function over for loop

--- challenges/common-array-elements/script.js
+++ challenges/common-array-elements/script.js
@@ -1,14 +1,23 @@
 function commonArrayElements(array1, array2){
   var results = [];
-  for (var i = 0; i < array1.length; ++i){
+  var newArray = array1.map(function(arrayScan){
+    array2.filter(function(checkArray2){
+      if (checkArray2 === arrayScan)
+      results.push(arrayScan)
+    });
+  });
+  return results
+}
+/*  for (var i = 0; i < array1.length; ++i){
     array2.filter(function(arrayCheck){
       if (arrayCheck === array1[i]){
-        results.push(arrayCheck);
+        results.pop(arrayCheck);
       };
     })
   }
   return results;
 }
+*/
 var array1 = [1, 20, 10, 100, 2];
 var array2 = [500, 2, 20, 200];
 var resultArray = commonArrayElements(array1, array2);
