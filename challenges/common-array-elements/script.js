138ae0e2c37a9237a95a281870aa547727cc72f2
tomdaniels <thomsondaniels@hotmail.com>
2017-06-26 15:21:17 +1000

    fixes pop, accidentally pushed

--- challenges/common-array-elements/script.js
+++ challenges/common-array-elements/script.js
@@ -8,16 +8,6 @@ function commonArrayElements(array1, array2){
   });
   return results
 }
-/*  for (var i = 0; i < array1.length; ++i){
-    array2.filter(function(arrayCheck){
-      if (arrayCheck === array1[i]){
-        results.pop(arrayCheck);
-      };
-    })
-  }
-  return results;
-}
-*/
 var array1 = [1, 20, 10, 100, 2];
 var array2 = [500, 2, 20, 200];
 var resultArray = commonArrayElements(array1, array2);
