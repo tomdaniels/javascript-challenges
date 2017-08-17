4c8431996e5d379070148cda1258ae2560840c5b
tomdaniels <thomsondaniels@hotmail.com>
2017-08-17 16:16:39 +1000

    cleans up the function to remove duplicates

--- challenges/remove-the-duplicates/script.js
+++ challenges/remove-the-duplicates/script.js
@@ -1,11 +1,11 @@
 function noDuplicates(array1, array2) {
-	var myarr = array1.concat(array2).sort(function(value1, value2) {
+	var myarr = array1.concat(array2);
+	var results = myarr.filter(function(item, index, self) {
+		return index == self.indexOf(item);
+	});
+	return results.sort(function(value1, value2) {
 		return (value1 - value2);
 	});
-	var results = myarr.filter(function(item, pos, ary) {
-		return !pos || item != ary[pos - 1];
-	});
-	return results;
 }
 
 var array1 = [10, 18, 1, 4, 7, 22];
